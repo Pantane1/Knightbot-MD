32c74b75b9748df83b6ddeaca180b4b593b980d1
Pantane1 <164737531+Pantane1@users.noreply.github.com>
2025-04-05 07:37:53 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'pantane',
+  author: '@pantane',
+  botName: "Pantane_md",
+  botOwner: 'Pantane Technest', // Your name
+  ownerNumber: '254740312402', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
